db44b7bc716eb51ec638c94bdcf2a021aa0aa8fa
Chanyeong Cho <cyjo9603@naver.com>
2020-05-20 18:07:07 +0900

    [refactor] remove checkData

--- js/util.js
+++ js/util.js
@@ -28,20 +28,6 @@ const util = {
   },
 
   /**
-   * checkData
-   * 두 배열의 원소들을 비교해 같은 값의 count를 반환
-   */
-  checkData: () => {
-    let check = 0;
-    for (let i = 0; i < gameData.mode; i++) {
-      for (let j = 0; j < gameData.mode; j++) {
-        if (gameData.table.before[i][j] === gameData.table.current[i][j]) check++;
-      }
-    }
-    return check;
-  },
-
-  /**
    * getRandomNumber
    * 0~number범위에서 랜덤한 정수값을 반환
    */
